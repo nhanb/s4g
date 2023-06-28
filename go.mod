61e4937143409b29cefe0ba2b74e06a2cc4f7c52
Nhân <hi@imnhan.com>
2023-06-28 21:04:04 +0700

    site skeleton and WIP findPosts()
    Should probably collect pages in the same walkdir too

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go.imnhan.com/webmaker2000
 
 go 1.20
+
+require github.com/BurntSushi/toml v1.3.2
--- main.go
+++ main.go
@@ -2,27 +2,68 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"io/fs"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
 )
 
 func main() {
 	var port, folder string
-	flag.StringVar(&port, "port", "3338", "Web port")
-	flag.StringVar(&folder, "folder", "www", "Web port")
+	flag.StringVar(&port, "port", "3338", "Port for local preview server")
+	flag.StringVar(&folder, "folder", "www", "Web folder")
 	flag.Parse()
 
-	err := os.Chdir(folder)
+	absolutePath, err := filepath.Abs(folder)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	fsys := os.DirFS(absolutePath)
+
+	meta := readSiteMetadata(fsys)
+	fmt.Println("Found site:", meta)
+
+	findPosts(fsys)
 
 	println("Serving local website at http://localhost:" + port)
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+	http.Handle("/", http.FileServer(http.FS(fsys)))
 	err = http.ListenAndServe("127.0.0.1:"+port, nil)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+}
+
+type SiteMetadata struct {
+	Name    string
+	Tagline string
+}
+
+func readSiteMetadata(fsys fs.FS) (sm SiteMetadata) {
+	_, err := toml.DecodeFS(fsys, "website.toml", &sm)
+	if err != nil {
+		panic(err)
+	}
+	return sm
+}
+
+type Article struct {
+	Path     string
+	Title    string
+	DjotBody string
+}
+type Post Article
+type Page Article
+
+func findPosts(fsys fs.FS) (posts []Post) {
+	var paths []string
+	fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.Name() == "post.toml" {
+			paths = append(paths, filepath.Dir(path))
+		}
+		return nil
+	})
+	return posts
 }
